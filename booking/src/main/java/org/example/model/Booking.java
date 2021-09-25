8e1d564ce73126341db7817442b8977f919e7a59
Võ Trần Minh Quân <deesatoshi@gmail.com>
2021-09-25 17:40:44 +0700

    refactor: remove @Table annotation in class Booking and make some changes in file application.properties

--- booking/src/main/java/org/example/model/Booking.java
+++ booking/src/main/java/org/example/model/Booking.java
@@ -6,27 +6,15 @@ import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "bookings")
 @Data
 public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
     private Long id;
-
-    @Column(name = "room_id")
     private Long roomId;
-
-    @Column(name = "customer_name")
     private String customerName;
-
-    @Column(name = "booking_date")
     private LocalDate bookingDate;
-
-    @Column(name = "checkin_date")
     private LocalDate checkinDate;
-
-    @Column(name = "checkout_date")
     private LocalDate checkoutDate;
 
     public Booking(Long roomId, String customerName, LocalDate bookingDate, LocalDate checkinDate, LocalDate checkoutDate) {
